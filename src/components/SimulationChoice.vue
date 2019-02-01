57f6014038a925dad72a11be1fc790baed8eb990
Enn Michael <enntheprogrammer@gmail.com>
2019-02-01 13:54:09 +0100

    Work so far

---
+++ src/components/SimulationChoice.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="options-root">
+    <select v-model="engineChoice">
+      <option value="massAggregate">
+        Mass Aggregate Engine
+      </option>
+      <option value="rigidBody">
+        Rigid Body Engine
+      </option>
+    </select>
+
+    <select v-if="" @change="$emit('update: demoChoice')">
+        <option v-for="">
+        </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            engineChoice: undefined,
+        };
+    },
+    watch: {
+        engineChoice(newChoice) {
+            this.$emit('update:engineChoice')
+        }
+    }
+};
+</script>
---
+++ src/components/mass-aggregate/ParticleDemo.vue
@@ -0,0 +1,21 @@
+<template>
+    <SimulationDisplay :simulation="simulation">
+    </SimulationDisplay>
+</template>
+
+<script>
+
+// TODO Simulation = engine + drawing
+// TODO How do we handle restarting now?
+
+export default {
+    data: {
+        simulation: new Simulation({
+            
+        }),
+    },
+    components: {
+        SimulationDisplay,
+    },
+}
+</script>
