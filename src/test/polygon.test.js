68d094bc9827723b36ad18e6e000c574096cd0b9
Enn Michael <enntheprogrammer@gmail.com>
2019-02-11 18:16:09 +0100

    Finished polygon tests, everything seems fine

--- src/test/polygon.test.js
+++ src/test/polygon.test.js
@@ -174,8 +174,41 @@ test('Polygon.containsPoint', () => {
                 new Vector(100, 100),
             ],
         },
-        // TODO Also test for a random polygon
-        // Could have a code generator thing for this, or not
+        {
+            polygon: Polygon.fromVertices([new Vector(-0.8, 0.8), new Vector(0.8, 0.8), new Vector(0, -0.8)]),
+            innerPoints: [
+                {
+                    point: new Vector(0.09534935573218895, -0.5203028966643934),
+                    closestEdges: [new Line({ origin: new Vector(0.8, 0.8), end: new Vector(0, -0.8) })],
+                },
+                {
+                    point: new Vector(-0.6362647941798485, 0.7933822353366304),
+                    closestEdges: [new Line({ origin: new Vector(-0.8, 0.8), end: new Vector(0.8, 0.8) })],
+                },
+                {
+                    point: new Vector(0.08678005586659565, 0.7846325542152781),
+                    closestEdges: [new Line({ origin: new Vector(-0.8, 0.8), end: new Vector(0.8, 0.8) })],
+                },
+                {
+                    point: new Vector(0.45230862833231367, 0.15588471403180493),
+                    closestEdges: [new Line({ origin: new Vector(0.8, 0.8), end: new Vector(0, -0.8) })]
+                },
+                {
+                    point: new Vector(-0.5298157111105166, 0.36197345923967017),
+                    closestEdges: [new Line({ origin: new Vector(0, -0.8), end: new Vector(-0.8, 0.8) })],
+                },
+                {
+                    point: new Vector(0.07273588857521762, -0.3468562911495727),
+                    closestEdges: [new Line({ origin: new Vector(0.8, 0.8), end: new Vector(0, -0.8) })],
+                },
+            ],
+            outerPoints: [
+                new Vector(-0.9865162598485082, -0.4274854332282385),
+                new Vector(0.20643739033344266, 0.9161476385333429),
+                new Vector(0.895309124143729, 0.6775310584845435),
+                new Vector(-0.569599271579202, -0.2601343847730464),
+            ],
+        }
     ];
 
     for (const { polygon, innerPoints, outerPoints } of testCases) {
