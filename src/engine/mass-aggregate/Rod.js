48df8e2cdcd7ef7ffaa25dc995e20579022919e9
Enn Michael <enntheprogrammer@gmail.com>
2019-02-11 22:00:52 +0100

    Particles connected by rods are now dragable. Fixes #8.

--- src/engine/mass-aggregate/Rod.js
+++ src/engine/mass-aggregate/Rod.js
@@ -2,6 +2,20 @@ import Spring from './Spring';
 
 export default class Rod extends Spring {
     contract(fromParticle, toParticle) {
+        if (fromParticle.fixed)
+            return;
+        if (toParticle.fixed)
+            this.contractOne(fromParticle, toParticle);
+        else
+            this.contractBoth(fromParticle, toParticle);
+    }
+
+    contractOne(fromParticle, toParticle) {
+        const direction = fromParticle.position.direction(toParticle.position);
+        fromParticle.position.add(direction.scaled(this.lengthDelta));
+    }
+
+    contractBoth(fromParticle, toParticle) {
         const direction = fromParticle.position.direction(toParticle.position);
         const netMass = fromParticle.mass + toParticle.mass;
         fromParticle.position.add(direction.scaled(this.lengthDelta * toParticle.mass / netMass));
